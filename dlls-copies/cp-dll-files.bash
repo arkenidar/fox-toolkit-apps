3b93e235138eb2b546c55a8911dfc33c5d84675c
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2023-11-21 13:13:35 +0100

    [setup, exe+dll] "dependency walker tool" used

---
+++ dlls-copies/cp-dll-files.bash
@@ -0,0 +1,21 @@
+# "dependency walker tool" used
+
+cp "c:\msys64\mingw64\bin\LIBSTDC++-6.DLL" .
+cp "c:\msys64\mingw64\bin\LIBWINPTHREAD-1.DLL" .
+cp "c:\msys64\mingw64\bin\LIBGCC_S_SEH-1.DLL" .
+cp "c:\msys64\mingw64\bin\ZLIB1.DLL" .
+cp "c:\msys64\mingw64\bin\LIBZSTD.DLL" .
+cp "c:\msys64\mingw64\bin\LIBSHARPYUV-0.DLL" .
+cp "c:\msys64\mingw64\bin\LIBWEBP-7.DLL" .
+cp "c:\msys64\mingw64\bin\LIBLZMA-5.DLL" .
+cp "c:\msys64\mingw64\bin\LIBLERC.DLL" .
+cp "c:\msys64\mingw64\bin\LIBJBIG-0.DLL" .
+cp "c:\msys64\mingw64\bin\LIBDEFLATE.DLL" .
+cp "c:\msys64\mingw64\bin\LIBTIFF-6.DLL" .
+cp "c:\msys64\mingw64\bin\LIBPNG16-16.DLL" .
+cp "c:\msys64\mingw64\bin\LIBJPEG-8.DLL" .
+cp "c:\msys64\mingw64\bin\LIBBZ2-1.DLL" .
+cp "c:\msys64\mingw64\bin\LIBFOX-1.7-0.DLL" .
+# cp "" .
+cp "c:\opt\dropbox\gh-repos\fox-toolkit-apps\build\GLTEST.EXE" .
+# cp "" .
